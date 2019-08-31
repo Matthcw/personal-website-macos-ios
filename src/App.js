e21eeda28c75e0a7c5df49026ce0005e30ffac6f
M Mukalere <matthcw@hotmail.co.uk>
2019-08-31 17:39:57 +0100

    modified styling

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ class App extends Component {
   render() {
     return (
       <div id="background">
+
         <header id="header">
-          {/* <span>:)</span> */}
-          <span>4:30am</span>
-          {/* <span>Matthew Mukalere</span> */}
+          <div>💪🏿️ Matthew Mukalere</div>
+          <div></div>
+          <div>Mon 4:30 AM</div>          
         </header>
+
+        <div id="main-body">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+        </div>
+          
+        <footer id="footer">
+          
+        </footer>
+
       </div>
     );
   }
