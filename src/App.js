ca5a08c0368efa427e8f2b964469f3911f4b0f2c
M Mukalere <matthcw@hotmail.co.uk>
2019-08-31 19:19:11 +0100

    added react-grid-layout for macos desktop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import GridLayout from 'react-grid-layout';
 import './App.css';
+import './styles-resize.css';
+import './styles-gridlayout.css';
 
 class App extends Component {
   render() {
@@ -14,8 +17,16 @@ class App extends Component {
         </header>
 
         <div id="main-body">
+          <div>
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <p>Welcome to React</p>
+          <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+        <div key="a" data-grid={{x: 0, y: 0, w: 1, h: 2, static: true}}>a</div>
+        <div key="b" data-grid={{x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4}}>b</div>
+        <div key="c" data-grid={{x: 4, y: 0, w: 1, h: 2}}>c</div>
+      </GridLayout>
+          </div>
+          
         </div>
           
         <footer id="footer">
