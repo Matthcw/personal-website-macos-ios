2d99be18e5b8a9135a647421e8b136d6fb589792
M Mukalere <matthcw@hotmail.co.uk>
2019-09-02 20:48:28 +0100

    improved grid layout stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import GridLayout from 'react-grid-layout';
+import Draggable from 'react-draggable';
 import './App.css';
 import './styles-resize.css';
 import './styles-gridlayout.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 1200
+    };
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
+  // componentDidMount() {
+  //   setInterval(() => {
+  //     this.setState(prevState => ({ width: prevState.width - 10 }))
+  //   }, 1000)
+  // }
+  updateDimensions() {
+
+    var w = window,
+      d = document,
+      documentElement = d.documentElement,
+      body = d.getElementsByTagName('body')[0],
+      width = w.innerWidth || documentElement.clientWidth || body.clientWidth,
+      height = w.innerHeight || documentElement.clientHeight || body.clientHeight;
+    
+    this.setState({ width: width, height: height });
+    // if you are using ES2015 I'm pretty sure you can do this: this.setState({width, height});
+  }
+  componentWillMount() {
+    this.updateDimensions();
+  }
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
+  onResize(layout, oldLayoutItem, layoutItem, placeholder) {
+    // `oldLayoutItem` contains the state of the item before the resize.
+    // You can modify `layoutItem` to enforce constraints.
+
+    if (layoutItem.h < 3 && layoutItem.w > 2) {
+      layoutItem.w = 2;
+      placeholder.w = 2;
+    }
+
+    if (layoutItem.h >= 3 && layoutItem.w < 2) {
+      layoutItem.w = 2;
+      placeholder.w = 2;
+    }
+  }
+
+
   render() {
     return (
       <div id="background">
@@ -13,24 +65,47 @@ class App extends Component {
         <header id="header">
           <div>💪🏿️ Matthew Mukalere</div>
           <div></div>
-          <div>Mon 4:30 AM</div>          
+          <div>Mon 4:30 AM</div>
         </header>
 
         <div id="main-body">
-          <div>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Welcome to React</p>
-          <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
-        <div key="a" data-grid={{x: 0, y: 0, w: 1, h: 2, static: true}}>a</div>
-        <div key="b" data-grid={{x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4}}>b</div>
-        <div key="c" data-grid={{x: 4, y: 0, w: 1, h: 2}}>c</div>
-      </GridLayout>
-          </div>
-          
+          <GridLayout
+            className="layout"
+            cols={12}
+            rowHeight={3}
+            width={this.state.width}
+            verticalCompact={false}>
+            <div key="b" data-grid={{ x: 0, y: 0, w: 1, h: 10 }}>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>My Portfolio</p>
+            </div>
+            <div key="c" data-grid={{ x: 4, y: 0, w: 1, h: 10 }}>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>About Me</p>
+            </div>
+          </GridLayout>
+
+          {/* <div id="draggableArea">
+              <Draggable
+                axis="both"
+                handle=".handle"
+                defaultPosition={{ x: 0, y: 0 }}
+                position={null}
+                grid={[25, 25]}
+                scale={1}
+                onStart={this.handleStart}
+                onDrag={this.handleDrag}
+                onStop={this.handleStop}>
+                <div>
+                  <div className="handle">Drag from here</div>
+                  <div>This readme is really dragging on...</div>
+                </div>
+              </Draggable>
+            </div> */}
         </div>
-          
+
         <footer id="footer">
-          
+
         </footer>
 
       </div>
